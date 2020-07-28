ed814f32596906ce91139168d23c26a83acda717
Abdul Rehman <Abdul Rehman@Abdul_Rehman>
2020-07-28 17:00:16 +0530

    first commit

---
+++ elf/src/test/java/com/elf/test/Createcust.java
@@ -0,0 +1,13 @@
+package com.elf.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Createcust {
+	@Test
+	public void createcust(){
+		Reporter.log("customer created",true);
+		
+	}
+
+}
